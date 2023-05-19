59dab26d31e676dbc5a69836a658395b5863454e
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2023-05-19 12:30:54 -0300

    response to test command working

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+
 use matrix_sdk::{
     Client, config::SyncSettings,
     room::Room,
-    ruma::{user_id, events::room::message::SyncRoomMessageEvent,
+    ruma::{user_id, events::room::message::{SyncRoomMessageEvent, OriginalSyncRoomMessageEvent, MessageType, RoomMessageEventContent},
            events::room::member::StrippedRoomMemberEvent, room_id, RoomId
           },
 };
 use tokio::time::{sleep, Duration};
 use dotenv::dotenv;
 
+
+// Async function that awaits for an invitation and accepts it automatically
 async fn on_stripped_state_member(
     room_member: StrippedRoomMemberEvent,
     client: Client,
@@ -57,6 +60,9 @@ async fn main() -> anyhow::Result<()> {
     // We add an event handler that listens if our user is invited to a room
     client.add_event_handler(on_stripped_state_member);
 
+    // We add an event handler that listens if our user is invited to a room
+    client.add_event_handler(on_room_message);
+
     // This event handler listens and prints every message it's received or sent
     client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
          println!("Received a message {:?}", ev);
@@ -67,4 +73,28 @@ async fn main() -> anyhow::Result<()> {
     client.sync(SyncSettings::default()).await?;
 
     Ok(())
+}
+
+// Async function that gets the text content of a room and answers if it matches. 
+// Cómo sabe de que room puede venir el mensaje: hay que pasarselo por parámetros
+async fn on_room_message(event: OriginalSyncRoomMessageEvent, room: Room) {
+    // First, we need to unpack the message: We only want messages from rooms we are
+    // still in and that are regular text messages - ignoring everything else.
+    let Room::Joined(room) = room else { return };
+    let MessageType::Text(text_content) = event.content.msgtype else { return };
+
+    // here comes the actual "logic": when the bot see's a `!party` in the message,
+    // it responds
+    if text_content.body.contains("!kusamaupdates") {
+        let content = RoomMessageEventContent::text_plain("🎉🎊🥳 I don't know how to get'em 🥳🎊🎉");
+
+        println!("sending");
+
+        // send our message to the room we found the "!party" command in
+        // the last parameter is an optional transaction id which we don't
+        // care about.
+        room.send(content, None).await.unwrap();
+
+        println!("message sent");
+    }
 }
